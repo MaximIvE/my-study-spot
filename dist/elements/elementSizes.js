02f4ebb126c492183846b6a378bd69fd4626892f
MaximIvE <ivanukmaxim@gmail.com>
2023-03-14 00:55:49 +0200

    Dist

---
+++ dist/elements/elementSizes.js
@@ -0,0 +1,8 @@
+"use strict";
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const headerPadding = document.querySelector(".header-padding");
+headerPadding.style.height = `${header.offsetHeight}px`;
+const content = document.querySelector("main");
+content.style.height = `${body.offsetHeight - header.offsetHeight}px`;
+//# sourceMappingURL=elementSizes.js.map
